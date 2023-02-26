b239c69da6e0ea3f78cb413250412fef8a7798b3
root <2935996123@qq.com>
2023-02-26 23:13:19 +0800

    命令行提交

--- serve/src/main/java/com/jiao/testproject/testproject/controller/newFile.java
+++ serve/src/main/java/com/jiao/testproject/testproject/controller/newFile.java
@@ -3,7 +3,7 @@ package com.jiao.testproject.testproject.controller;
 public class newFile {
 
 
-    String aaaa= "12356546 由git 分支提交 ";
+    String aaaa= "12356546 由git 分支提交  command commit";
 
 
 }
