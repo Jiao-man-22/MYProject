3e58e2284df5469b01beaca704aafa63887af712
dearxiaogiegie <2935996123@qq.com>
2023-05-11 17:44:17 +0800

    试试 video 播放视频

---
+++ web/src/component_new/videoPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <span @click="videoClick()"> 点击播放</span>
+   <span @click="videoPause()"> 点击暂停</span>
+    <video width="1000" controls="controls" ref="video">
+        <source src="../assets/video/1.mp4" type="video/mp4"/>
+        <object data="../assets/video/1.mp4" width="1000" height="1000">
+            你的浏览器不支持该格式
+        </object>
+    </video>
+  </div>
+</template>
+
+<script >
+export default {
+    data() {
+      return {
+      }
+    },
+    methods:{
+        videoClick(){
+            this.$refs.video.play();
+            this.isPlay = true
+        },
+        videoPause(){
+            this.$refs.video.pause();
+            this.isPlay = false
+        }
+    }
+}
+</script>
+<style>
+
+</style>
