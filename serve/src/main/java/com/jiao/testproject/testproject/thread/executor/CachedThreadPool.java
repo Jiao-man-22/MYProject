94edc045ed458d3eb414d3fd0c5bb0231bdb72f5
dearxiaogiegie <2935996123@qq.com>
2023-05-11 17:41:32 +0800

    多线程调用

---
+++ serve/src/main/java/com/jiao/testproject/testproject/thread/executor/CachedThreadPool.java
@@ -0,0 +1,18 @@
+package com.jiao.testproject.testproject.thread.executor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPool {
+    public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            executorService.execute(new LiftOff());
+            executorService.shutdown();
+           // executorService = Executors.newCachedThreadPool();
+        }
+
+    }
+}
